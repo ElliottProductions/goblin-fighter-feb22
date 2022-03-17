934a61d12172490c17772205d7418422bc04e704
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-17 11:14:37 -0800

    goblin form and button working to add goblins!

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const goblinArray = [
         hp: 5 }];
 
 const goblinContainer = document.getElementById('goblins');
+const goblinInput = document.getElementById('goblin-input');
+const newGobButton = document.getElementById('goblin-button');
+const goblinForm = document.getElementById('goblin-form');
 
 
 
@@ -19,11 +22,55 @@ for (let goober of goblinArray) {
 
 }
 
+newGobButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const data = goblinInput.value;
+
+    const newGob = {
+        name: data || `Goblin # ${Math.floor(Math.random() * 1000)}`,
+        hp: Math.floor(Math.random() * 5 + 1)};
+
+    goblinArray.push(newGob);
+    
+
+    displayGoblins();
+    
+    goblinForm.reset();
+});
+
+goblinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(goblinForm);
+
+    let nameValue = data.get('goblin-field');
+
+    const newGob = {
+        name: nameValue || `Goblin # ${Math.floor(Math.random() * 1000)}`,
+        hp: Math.floor(Math.random() * 5 + 1)};
+
+    goblinArray.push(newGob);
+   
+
+    displayGoblins();
+    
+    goblinForm.reset();
+});
+
+
+
+function displayGoblins() {
+    goblinContainer.innerHTML = '';
+
+    for (let goober of goblinArray) {
+        const newGob = renderGoblin(goober);
+
+        goblinContainer.append(newGob);
+    }
+
+}
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
-  // update DOM to reflect the new state
-
-
+  // update DOM to reflect the new stat
