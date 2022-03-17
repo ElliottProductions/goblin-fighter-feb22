d58fc8bb4da4c6fe9b5a3842f0e08b5a98497f6a
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-17 10:40:25 -0800

    goblins rendering

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // import functions and grab DOM elements
 import { renderGoblin } from './render-utils.js';
 
-const goblinArray = [{}, {}];
+const goblinArray = [
+    { name: 'Tapatio',
+        hp: 5 },
+    { name: 'Sriracha',
+        hp: 5 }];
 
 const goblinContainer = document.getElementById('goblins');
+
+
+
+for (let goober of goblinArray) {
+    
+    const gobEl = renderGoblin(goober);
+
+    goblinContainer.append(gobEl);
+
+}
 
 // let state
 
@@ -11,3 +25,5 @@ const goblinContainer = document.getElementById('goblins');
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
--- render-utils.js
+++ render-utils.js
@@ -2,6 +2,18 @@ export function renderGoblin(goober) {
     const newGob = document.createElement('div');
     newGob.classList.add('goblinwithclass');
 
+    const nameEl = document.createElement('p');
+    const hpEl = document.createElement('p');
     
+    
+    
+    nameEl.textContent = goober.name;
+    hpEl.textContent = goober.hp;
+
+    newGob.append(nameEl, hpEl);
+
+    return newGob;
+
+
 
 }
